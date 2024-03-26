fc8d93ac67bb7a90ff41bd0fee35cec9df70b4b6
Mizakson <maxwellizakson@gmail.com>
2024-03-26 10:18:34 -0400

    Edit script.js main docstring

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ weather api key:
 for the purpose of this project, 
 this free api key is in the source code on github
 
+free-tier limits forecast data to 3 days
+
 */
 
 const WEATHER_API_KEY = "9740515f7e2f4924acb162332242503"
