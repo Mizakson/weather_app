790279f7a8dcef584568a04c0cda6d50e019d1cf
Mizakson <maxwellizakson@gmail.com>
2024-03-26 12:05:51 -0400

    Create searchLocation function, add button event

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ https://api.weatherapi.com/v1/forecast.json?key=keyHere&q=locationHere
 */
 
 const WEATHER_API_KEY = "9740515f7e2f4924acb162332242503"
+const searchField = document.getElementById("search")
+const searchBtn = document.querySelector(".main-search")
+
+function searchLocation(location) {
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location}&days=1`, {mode: "cors"})
+    .then(function(data) {
+        return data.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault()
+    const location = searchField.value
+    searchLocation(location)
+})
