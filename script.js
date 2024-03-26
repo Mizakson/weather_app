10e812f7fcc62fbeaed25c01e6b23f732c6651ab
Mizakson <maxwellizakson@gmail.com>
2024-03-26 10:17:04 -0400

    Add free api key to script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/* 
+weather api key:
+9740515f7e2f4924acb162332242503
 
+for the purpose of this project, 
+this free api key is in the source code on github
+
+*/
+
+const WEATHER_API_KEY = "9740515f7e2f4924acb162332242503"
