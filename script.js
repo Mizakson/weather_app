93d5cb1628f6c37a5f1d6f7c7e35e55c7f13ae8f
Mizakson <maxwellizakson@gmail.com>
2024-03-26 10:24:26 -0400

    Add weather api request url psuedocode

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ this free api key is in the source code on github
 
 free-tier limits forecast data to 3 days
 
+request url psuedocode
+https://api.weatherapi.com/v1/forecast.json?key=keyHere&q=locationHere
+
 */
 
 const WEATHER_API_KEY = "9740515f7e2f4924acb162332242503"
