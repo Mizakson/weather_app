6a83946e01ee909ce31a7254f8e878247d340f26
Mizakson <maxwellizakson@gmail.com>
2024-03-26 11:47:55 -0400

    Hardcode project data elements (index.html)

--- script.js
+++ script.js
@@ -13,29 +13,3 @@ https://api.weatherapi.com/v1/forecast.json?key=keyHere&q=locationHere
 */
 
 const WEATHER_API_KEY = "9740515f7e2f4924acb162332242503"
-
-function getWeatherData(location) {
-
-  fetch(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location}&days=1`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(response) {
-        
-        if (response.current.condition.code === 1006) {
-            throw new Error("INVALID LOCATION")
-        }
-
-        if (response.current.condition.code === 2007) {
-            throw new Error("MONTHLY CALL LIMIT EXCEEDED")
-        }
-
-        else {
-            console.log(response)
-        }
-
-    })
-
-}
-
-getWeatherData("tampa")
